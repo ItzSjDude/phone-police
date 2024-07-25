0a9303aa5559a9c5154405690acafec27f051c56
chankruze <chankruze@gmail.com>
2024-07-25 17:47:27 +0530

    ui(app): start implementing full charge activity
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/src/main/java/com/geekofia/phonepolice/activities/HomeActivity.java
+++ app/src/main/java/com/geekofia/phonepolice/activities/HomeActivity.java
@@ -13,34 +13,29 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.geekofia.phonepolice.adapters.CardAdapter;
+import com.geekofia.phonepolice.adapters.SafetyFeatureCardAdapter;
 import com.geekofia.phonepolice.R;
 import com.geekofia.phonepolice.databinding.ActivityHomeBinding;
-import com.geekofia.phonepolice.models.CardItem;
+import com.geekofia.phonepolice.models.SafetyFeatureCardItem;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private ActivityHomeBinding binding;
     private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
-    private RecyclerView recyclerView;
-    private CardAdapter cardAdapter;
-    private List<CardItem> cardItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        com.geekofia.phonepolice.databinding.ActivityHomeBinding binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(binding.toolbar);
 
         drawerLayout = binding.drawerLayout;
-        navigationView = binding.navigationView;
+        NavigationView navigationView = binding.navigationView;
         navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -49,17 +44,17 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         binding.recyclerView.setLayoutManager(new GridLayoutManager(this, 2)); // 2 columns
 
-        cardItemList = new ArrayList<>();
+        List<SafetyFeatureCardItem> safetyFeatureCardItemList = new ArrayList<>();
         // Add card items to the list
-        cardItemList.add(new CardItem("Intruder Alert", R.drawable.ic_intruder, R.drawable.bg_intruder_card));
-        cardItemList.add(new CardItem("Anti Touch Detection", R.drawable.ic_anti_touch, R.drawable.bg_anti_touch_card));
-        cardItemList.add(new CardItem("Wrong Password Alert", R.drawable.ic_wrong_password, R.drawable.bg_wrong_pass));
-        cardItemList.add(new CardItem("Charging Removal Alert", R.drawable.ic_charging_removal, R.drawable.bg_charger_removal_card));
-        cardItemList.add(new CardItem("Full Battery Alert", R.drawable.ic_full_battery, R.drawable.bg_battery_full_card));
-        cardItemList.add(new CardItem("Pocket Alarm", R.drawable.ic_pocket_alarm, R.drawable.bg_pocket_alarm));
+        safetyFeatureCardItemList.add(new SafetyFeatureCardItem("Intruder Alert", R.drawable.ic_intruder, null));
+        safetyFeatureCardItemList.add(new SafetyFeatureCardItem("Anti Touch Detection", R.drawable.ic_anti_touch, null));
+        safetyFeatureCardItemList.add(new SafetyFeatureCardItem("Wrong Password Alert", R.drawable.ic_wrong_password, null));
+        safetyFeatureCardItemList.add(new SafetyFeatureCardItem("Charging Removal Alert", R.drawable.ic_charging_removal, null));
+        safetyFeatureCardItemList.add(new SafetyFeatureCardItem("Full Battery Alert", R.drawable.ic_full_battery, FullChargeActivity.class));
+        safetyFeatureCardItemList.add(new SafetyFeatureCardItem("Pocket Alarm", R.drawable.ic_pocket_alarm, null));
 
-        cardAdapter = new CardAdapter(this, cardItemList);
-        binding.recyclerView.setAdapter(cardAdapter);
+        SafetyFeatureCardAdapter safetyFeatureCardAdapter = new SafetyFeatureCardAdapter(this, safetyFeatureCardItemList);
+        binding.recyclerView.setAdapter(safetyFeatureCardAdapter);
     }
 
     @Override
