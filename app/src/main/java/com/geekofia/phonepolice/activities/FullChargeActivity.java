634028fe25db73d0b6e1d7ca0315951efe553732
chankruze <chankruze@gmail.com>
2024-07-25 20:26:10 +0530

    core(full-charge-activity): sync shared preferences
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/src/main/java/com/geekofia/phonepolice/activities/FullChargeActivity.java
+++ app/src/main/java/com/geekofia/phonepolice/activities/FullChargeActivity.java
@@ -1,17 +1,23 @@
 package com.geekofia.phonepolice.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 import com.geekofia.phonepolice.R;
 import com.geekofia.phonepolice.databinding.ActivityFullChargeBinding;
 
-public class FullChargeActivity extends AppCompatActivity {
+public class FullChargeActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     private ActivityFullChargeBinding binding;
+    private static final String SWITCH_KEY = "full_battery_alert";
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,15 @@ public class FullChargeActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.preference_container, new ActivityFullChargeSettingsFragment())
                 .commit();
+
+        // Get the default shared preferences
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // Register the preference change listener
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+        // Check the current state of the switch preference
+        updateToggleState(sharedPreferences.getBoolean(SWITCH_KEY, false));
     }
 
     @Override
@@ -46,11 +61,39 @@ public class FullChargeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, @Nullable String key) {
+        if (SWITCH_KEY.equals(key)) {
+            // Handle the change in the switch preference
+            boolean isEnabled = sharedPreferences.getBoolean(SWITCH_KEY, false);
+            updateToggleState(isEnabled);
+        }
+    }
+
+    private void updateToggleState(boolean isEnabled) {
+        // Perform actions based on the toggle state
+        if (isEnabled) {
+            // Logic when the switch is enabled
+            Toast.makeText(this, "Feature enabled", Toast.LENGTH_SHORT).show();
+        } else {
+            // Logic when the switch is disabled
+            Toast.makeText(this, "Feature disabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     public static class ActivityFullChargeSettingsFragment extends PreferenceFragmentCompat {
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.preferences_activity_full_charge, rootKey);
         }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unregister the preference change listener to avoid memory leaks
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 }
